92557904abe4f6975d699840e0e35604e24884e6
Roman <user@email.com>
2023-07-12 18:36:25 +0500

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2) {
+    Console.WriteLine($"Числа {num1} и {num2} равны ");
+    return;
+}
+
+if (num1 > num2) {
+    Console.WriteLine($"Число {num1} больше, чем {num2} ");
+} else {
+    Console.WriteLine($"Число {num2} больше, чем {num1} ");
+} 
